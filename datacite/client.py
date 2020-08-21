832800b2062dfb35ce6a994bd6b918ce18426e98
asencis <70026705+asencis@users.noreply.github.com>
2020-08-21 16:37:50 +0100

    Add files via upload
    Added the DataCite RESTful API client wrapper, versioning for the package and a validate.py process for validating serialized data with a pre-set schema.json as DataCite Metadata Schema 4.2 (datacite-schema4.2, see https://schema.datacite.org/meta/kernel-4.2/).

---
+++ datacite/client.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of asencis.
+#
+# Copyright (C) 2020 asencis.
+#
+# This is free software; you can redistribute it and/or modify it
+# under the terms of the Revised BSD License; see LICENSE file for
+# more details.
+
+"""
+    Python API wrapper for the DataCite RESTful API service
+    Using: v.2.0: January 7, 2019, new API endpoints, using camelCase for attributes
+    Docs: [https://support.datacite.org/reference/introduction, https://support.datacite.org/docs/api]
+"""
+
+import json
+import requests
+
+from requests.auth import HTTPBasicAuth
+from requests.exceptions import RequestException
+
+class DataciteRESTAPIClient(object):
+
+    """
+    DataCite RESTful API client wrapper.
+    Docs: [https://support.datacite.org/reference/introduction]
+    The DataCite REST API returns information about DataCite content.
+    The API is RESTFUL and returns results in JSON.
+    It follows the JSONAPI specification.
+    The mime-type for API results is application/vnd.api+json.
+    """
+
+    def __init__(self, url, username, password):
+        """
+        The base DataciteRESTAPIClient class
+
+        @param username: DataCite username.
+        @param password: DataCite password.
+        @param url: DataCite RESTful API base URL. Defaults to https://api.datacite.org/.
+
+        Returns object with appended url, username and password attributes.
+        """
+        self.url = url
+        self.headers = {'content-type': 'application/vnd.api+json'}
+        self.action = None
+        self.username = username
+        self.password = password
+
+    def create_or_update_doi(self, data, doi):
+        """"
+        Method to "mint" a new DOI, or update it if it already exists.
+
+        @param doi: DOI name for the target resource.
+        @param data: this is the body payload which is a JSON serialized version of the DOI object
+
+        Returns a tuple of created, update Boolean flags and response object
+        """
+
+        self.action = 'create_doi'
+
+        response = None
+
+        created = False
+
+        updated = False
+
+        try:
+            response = requests.post(
+                '{}/dois'.format(self.url),
+                data=data,
+                headers=self.headers,
+                auth=HTTPBasicAuth(self.username, self.password),
+            )
+
+            response.raise_for_status()
+
+            created = True
+
+        except requests.exceptions.HTTPError as exception:
+            if response.status_code == 422:
+                response = requests.put(
+                    '{}/dois/{}'.format(self.url, doi),
+                    data=data,
+                    headers=self.headers,
+                    auth=HTTPBasicAuth(self.username, self.password),
+                )
+
+                response.raise_for_status()
+
+                updated = True
+
+        return created, updated, response
+
+    def delete(self, doi):
+        """"
+        Method to deleted a DOI if it exists.
+
+        @param doi: DOI name for the target resource.
+
+        Returns a tuple of deleted Boolean flag and response object
+        """
+
+        self.action = 'delete_doi'
+
+        response = None
+
+        deleted = False
+
+        try:
+            response = requests.delete(
+                '{}/dois/{}'.format(self.url, doi),
+                headers=headers,
+                auth=HTTPBasicAuth(self.username, self.password),
+            )
+
+            response.raise_for_status()
+
+            deleted = True
+
+        except requests.exceptions.HTTPError as exception:
+            deleted = False
+
+        return deleted, response
