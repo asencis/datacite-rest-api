0b70534c75763e98fc7e84afa45b661569192c14
asencis <michael@hiyield.co.uk>
2020-08-26 16:19:41 +0100

    Updates to the DataciteRESTAPIClient class to include the retrive_doi() class method call.

---
+++ validate.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of asencis.
+#
+# Copyright (C) 2020 asencis.
+#
+# This is free software; you can redistribute it and/or modify it
+# under the terms of the Revised BSD License; see LICENSE file for
+# more details.
+import os
+import json
+
+from jsonschema import validate
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+def validate_doi_schema42(instance):
+    jsonschema_42 = os.path.join(BASE_DIR, 'datacite/schemas/datacite-schema4.2.json')
+    with open(jsonschema_42) as schema:
+        schema = json.loads(schema.read())
+
+    validate(instance=instance, schema=schema)
